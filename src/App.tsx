1d51cf9996bafb1410c85e3be36c4a93042f4e99
Vladimir <homie43@yandex.ru>
2023-09-22 16:18:16 +0300

    add array users in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
-import { userSlice } from './redux/reducers/UserSlice';
 import { fetchUsers } from './redux/reducers/ActionCreators';
 
 function App() {
     const dispatch = useAppDispatch();
-    const { users } = useAppSelector((state) => state.userReducer);
+    const { users, isLoading, error } = useAppSelector(
+        (state) => state.userReducer
+    );
 
     React.useEffect(() => {
         dispatch(fetchUsers());
     }, []);
 
-    return <div className='App'>{JSON.stringify(users, null, 2)}</div>;
+    return (
+        <div className='App'>
+            {isLoading && <h1>Идет загрузка...</h1>}
+            {error && <h1>Ошибка</h1>}
+            {JSON.stringify(users, null, 2)}
+        </div>
+    );
 }
 
 export default App;

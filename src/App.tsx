965db95c092017c9f6f1a0768ba59666b081f016
Vladimir <homie43@yandex.ru>
2023-09-22 16:11:59 +0300

    create action creators

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './redux/reducers/UserSlice';
+import { fetchUsers } from './redux/reducers/ActionCreators';
 
 function App() {
     const dispatch = useAppDispatch();
+    const { users } = useAppSelector((state) => state.userReducer);
 
-    return <div className='App'></div>;
+    React.useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+    return <div className='App'>{JSON.stringify(users, null, 2)}</div>;
 }
 
 export default App;
--- src/redux/reducers/ActionCreators.ts
+++ src/redux/reducers/ActionCreators.ts
@@ -2,12 +2,19 @@ import { IUser } from '../../models/IUser';
 import { AppDispatch } from '../store';
 
 import axios from 'axios';
+import { userSlice } from './UserSlice';
 
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
     // возвращаем сначала dispatch, и уже из нее будет производить асинхронные дейтсвия
     try {
-        const response = await axios.get<IUser>(
+        dispatch(userSlice.actions.usersFetching());
+        const response = await axios.get<IUser[]>(
             'https://jsonplaceholder.typicode.com/users'
         );
-    } catch (e) {}
+        dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+    } catch (e) {
+        if (e instanceof Error) {
+            dispatch(userSlice.actions.usersFetchingError(e.message));
+        }
+    }
 };
